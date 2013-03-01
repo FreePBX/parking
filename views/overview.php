a33b86e292a83f08ae71126924bfbb5a0bf42dc9
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2013-03-01 21:04:21 +0000

    fixes missed localization

--- views/overview.php
+++ views/overview.php
@@ -1,4 +1,4 @@
-This module is used to configure Parking Lot(s) in Asterisk.
+<?php echo _("This module is used to configure Parking Lot(s) in Asterisk.") ?>
 <br/><br/>
-<div width="85%">Simply transfer the call to said parking lot extension. Asterisk will then read back the parking lot number the call has been placed in. To retrieve the call simply dial that number back.</div>
+<div width="85%"><?php _("Simply transfer the call to said parking lot extension. Asterisk will then read back the parking lot number the call has been placed in. To retrieve the call simply dial that number back.") ?></div>
 <?php if(function_exists('parking_overview_display')) { echo parking_overview_display(); }?>
