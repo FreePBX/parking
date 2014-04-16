ceeac66e549e7011a22453cee4cf45f08d8a8e1e
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-16 15:24:37 -0500

    update License

--- functions.inc.php
+++ functions.inc.php
@@ -1,8 +1,8 @@
 <?php
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed');}
-/*
-* * Copyright 2013 by Astrogen LLC
-* */
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright 2013 Schmooze Com Inc.
+//
 
 	include_once(dirname(__FILE__) . '/functions.inc/registries.php');
 	include_once(dirname(__FILE__) . '/functions.inc/geters_seters.php');
