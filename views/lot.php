1ed59f1298de10c76b788927e2f64dafddfb2775
Andrew Nagy <andrew.nagy@the159.com>
2015-12-28 19:06:40 -0800

    support Alert Info drop down complete

--- views/lot.php
+++ views/lot.php
@@ -378,7 +378,7 @@ if(function_exists('recordings_list')) { //only include if recordings is enabled
 							<i class="fa fa-question-circle fpbx-help-icon" data-for="alertinfo"></i>
 						</div>
 						<div class="col-md-9">
-							<input type="text" class="form-control" id="alertinfo" name="alertinfo" value="<?php echo htmlspecialchars($alertinfo)?>">
+							<?php echo FreePBX::View()->alertInfoDrawSelect("alertinfo",htmlspecialchars($alertinfo));?>
 						</div>
 					</div>
 				</div>
