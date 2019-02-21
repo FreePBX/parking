56b1ba751f1f880b48496dfd67e9f7ffde4cf52a
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 18:45:37 -0500

    [Module Tag script: parking 15.0.6] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -2,24 +2,13 @@
 namespace FreePBX\modules\Parking;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-      $configs = reset($this->getConfigs());
-      foreach ($configs as $lot) {
-        $this->FreePBX->Parking->save($lot);
-      }
-  }
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $tables = array_flip($tables + $unknownTables);
-    if (!isset($tables['parkpro'])) {
-      return $this;
-    }
-    $cb = $this->FreePBX->Parking;
-    $cb->setDatabase($pdo);
-    $configs = $cb->getAllParkingLots();
-    $cb->resetDatabase();
-    foreach ($configs as $lot) {
-      $cb->save($lot);
-    }
-    return $this;
-  }
+	public function runRestore($jobid){
+			$configs = reset($this->getConfigs());
+			foreach ($configs as $lot) {
+				$this->FreePBX->Parking->save($lot);
+			}
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
+		$this->restoreLegacyDatabase($pdo);
+	}
 }
